fa113a9ed5a04d95a5a03c2a00b080f59ecf43e9
ligi <ligi@ligi.de>
2017-06-22 16:19:11 +0200

    Addresses are the same when hex is the same ignoring the case

--- model/src/main/java/org/kethereum/model/Address.kt
+++ model/src/main/java/org/kethereum/model/Address.kt
@@ -1,3 +1,9 @@
 package org.kethereum.model
 
-data class Address(val hex: String)
+data class Address(val hex: String) {
+
+    override fun equals(other: Any?)
+            = other is Address && other.hex.toUpperCase() == hex.toUpperCase()
+
+    override fun hashCode() = hex.toUpperCase().hashCode()
+}
