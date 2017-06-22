a295cb4df0fdc0259640d917bfb830f4c6937f24
ligi <ligi@ligi.de>
2017-06-22 17:03:01 +0200

    Expose function

--- model/src/main/java/org/kethereum/functions/HexFun.kt
+++ model/src/main/java/org/kethereum/functions/HexFun.kt
@@ -8,7 +8,7 @@ private val CHARS = "0123456789abcdef"
 /**
  *  Returns 2 char hex string for Byte
  */
-internal fun Byte.toHexString() = toInt().let {
+fun Byte.toHexString() = toInt().let {
     CHARS[it.shr(4) and 0x0f].toString() + CHARS[it.and(0x0f)].toString()
 }
 
