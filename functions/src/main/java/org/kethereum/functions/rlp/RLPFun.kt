20e2265ba6ec2805a3cae148f610b607ce006b14
ligi <ligi@ligi.de>
2017-07-05 01:12:17 +0200

    Implement RLP encoder

---
+++ functions/src/main/java/org/kethereum/functions/rlp/RLPFun.kt
@@ -0,0 +1,33 @@
+package org.kethereum.functions.rlp
+
+/**
+RLP as of Appendix B. Recursive Length Prefix at https://github.com/ethereum/yellowpaper
+ */
+
+val element_offset = 128
+val list_offset = 192
+
+fun RLPType.encode(): ByteArray = when (this) {
+    is RLPElement -> bytes.encode(element_offset)
+    is RLPList -> element.map { it.encode() }
+            .fold(ByteArray(0), { acc, bytes -> acc.plus(bytes) }) // this can be speed optimized when needed
+            .encode(list_offset)
+    else -> throw (IllegalArgumentException("RLPType must be RLPElement or RLPList"))
+}
+
+private fun ByteArray.encode(offset: Int) = when {
+    size == 1 && (first().toInt() and 0xff < 128) && offset == element_offset -> this
+    size <= 55 -> ByteArray(1, { (size + offset).toByte() }).plus(this)
+    else -> size.toMinimalByteArray().let { arr ->
+        ByteArray(1, { (offset + 0x37 + arr.size).toByte() })
+                .plus(arr)
+                .plus(this)
+    }
+}
+
+fun String.toRLP() = RLPElement(toByteArray())
+fun Int.toRLP() = RLPElement(toMinimalByteArray())
+
+fun Int.toByteArray() = ByteArray(4, { i -> shr(8 * (3 - i)).toByte() })
+internal fun Int.toMinimalByteArray() = toByteArray().let { it.copyOfRange(it.minimalStart(), 4) }
+private fun ByteArray.minimalStart() = indexOfFirst { it != 0.toByte() }.let { if (it == -1) 4 else it }
---
+++ functions/src/main/java/org/kethereum/functions/rlp/RLPModel.kt
@@ -0,0 +1,20 @@
+package org.kethereum.functions.rlp
+
+import java.util.*
+
+/**
+RLP as of Appendix B. Recursive https://github.com/ethereum/yellowpaper
+ */
+
+interface RLPType
+
+data class RLPElement(val bytes: ByteArray) : RLPType {
+    override fun equals(other: Any?) = when (other) {
+        is RLPElement -> Arrays.equals(bytes, other.bytes)
+        else -> false
+    }
+
+    override fun hashCode() = Arrays.hashCode(bytes)
+}
+
+data class RLPList(val element: List<RLPType>) : RLPType
