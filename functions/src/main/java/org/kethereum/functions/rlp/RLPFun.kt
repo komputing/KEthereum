52422fcd214aea58022f8e794011f691e08bbd53
ligi <ligi@ligi.de>
2017-07-05 15:21:49 +0200

    Use operator overloading

--- functions/src/main/java/org/kethereum/functions/rlp/RLPFun.kt
+++ functions/src/main/java/org/kethereum/functions/rlp/RLPFun.kt
@@ -10,7 +10,7 @@ val list_offset = 192
 fun RLPType.encode(): ByteArray = when (this) {
     is RLPElement -> bytes.encode(element_offset)
     is RLPList -> element.map { it.encode() }
-            .fold(ByteArray(0), { acc, bytes -> acc.plus(bytes) }) // this can be speed optimized when needed
+            .fold(ByteArray(0), { acc, bytes -> acc + bytes }) // this can be speed optimized when needed
             .encode(list_offset)
     else -> throw (IllegalArgumentException("RLPType must be RLPElement or RLPList"))
 }
@@ -19,9 +19,7 @@ private fun ByteArray.encode(offset: Int) = when {
     size == 1 && (first().toInt() and 0xff < 128) && offset == element_offset -> this
     size <= 55 -> ByteArray(1, { (size + offset).toByte() }).plus(this)
     else -> size.toMinimalByteArray().let { arr ->
-        ByteArray(1, { (offset + 0x37 + arr.size).toByte() })
-                .plus(arr)
-                .plus(this)
+        ByteArray(1, { (offset + 0x37 + arr.size).toByte() }) + arr + this
     }
 }
 
