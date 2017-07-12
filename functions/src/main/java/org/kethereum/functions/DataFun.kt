5144db14b9215a681b473b3a80524df99e973cb0
ligi <ligi@ligi.de>
2017-07-12 16:37:41 +0200

    Improve startsWith Method

--- functions/src/main/java/org/kethereum/functions/DataFun.kt
+++ functions/src/main/java/org/kethereum/functions/DataFun.kt
@@ -1,11 +1,5 @@
 package org.kethereum.functions
 
-fun List<Byte>.startsWith(prefix: List<Byte>): Boolean {
-    if (prefix.size > this.size)
-        return false
-    (0..(prefix.size) - 1).forEach {
-        if (this[it] != prefix[it])
-            return false
-    }
-    return true
-}
+fun List<Byte>.startsWith(prefix: List<Byte>)
+        = size >= prefix.size &&
+        (0..(prefix.size - 1)).all { this[it] == prefix[it] }
---
+++ functions/src/test/java/org/kethereum/functions/TheDataFun.kt
@@ -0,0 +1,25 @@
+package org.kethereum.functions
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+class TheDataFun {
+
+    @Test
+    fun testStartsWith() {
+        assertThat(listOf(0.toByte(),1.toByte()).startsWith(listOf(0.toByte())))
+                .isTrue()
+        assertThat(listOf(0.toByte(),1.toByte()).startsWith(listOf(0.toByte(),1.toByte())))
+                .isTrue()
+        assertThat(listOf<Byte>().startsWith(listOf<Byte>()))
+                .isTrue()
+
+        assertThat(listOf(0.toByte(),1.toByte()).startsWith(listOf(2.toByte(),1.toByte())))
+                .isFalse()
+        assertThat(listOf(0.toByte(),1.toByte()).startsWith(listOf(3.toByte())))
+                .isFalse()
+        assertThat(listOf(0.toByte()).startsWith(listOf(2.toByte(),1.toByte())))
+                .isFalse()
+    }
+
+}
