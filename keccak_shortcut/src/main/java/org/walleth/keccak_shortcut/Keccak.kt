7f1ce176daa8d173cba45c811e00918606c7b75f
ligi <ligi@ligi.de>
2017-07-16 13:11:15 +0200

    Add keccak shortcut for ByteArray

--- keccak_shortcut/src/main/java/org/walleth/keccak_shortcut/Keccak.kt
+++ keccak_shortcut/src/main/java/org/walleth/keccak_shortcut/Keccak.kt
@@ -4,3 +4,4 @@ import org.walleth.keccak.Keccak
 import org.walleth.keccak.Parameter.KECCAK_256
 
 fun String.keccak() = Keccak().getHash(this, KECCAK_256)
+fun ByteArray.keccak() = Keccak().getHash(this, KECCAK_256)
