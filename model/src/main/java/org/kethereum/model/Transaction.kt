e14398ba734447bd4c9a4f7f9dbf7c4a2d768b7f
ligi <ligi@ligi.de>
2017-07-16 13:11:35 +0200

    Remove sigHash

--- model/src/main/java/org/kethereum/model/Transaction.kt
+++ model/src/main/java/org/kethereum/model/Transaction.kt
@@ -7,12 +7,11 @@ import java.math.BigInteger
 data class Transaction(val value: BigInteger,
                        val from: Address,
                        val to: Address?,
-
                        val creationEpochSecond: Long? = null,
+
                        var nonce: BigInteger? = null,
                        var gasPrice: BigInteger = DEFAULT_GAS_PRICE,
                        var gasLimit: BigInteger = DEFAULT_GAS_LIMIT,
-                       var sigHash: String? = null,
                        var txHash: String? = null,
                        var input: List<Byte> = emptyList(),
                        var signatureData: SignatureData? = null)
