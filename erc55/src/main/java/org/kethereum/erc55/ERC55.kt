7ee9f008fe017458bc236897233a2b3f8a18121c
ligi <ligi@ligi.de>
2017-10-14 14:20:21 +0200

    Adapt to changes

--- erc55/src/main/java/org/kethereum/erc55/ERC55.kt
+++ erc55/src/main/java/org/kethereum/erc55/ERC55.kt
@@ -1,20 +1,22 @@
 package org.kethereum.erc55
 
 import org.kethereum.functions.isValid
+import org.kethereum.keccakshortcut.keccak
 import org.kethereum.model.Address
-import org.walleth.keccak_shortcut.keccak
 import org.walleth.khex.fromHexToInt
+import org.walleth.khex.toHexString
 
 /*
 ERC-55 Checksum as in https://github.com/ethereum/EIPs/blob/master/EIPS/eip-55.md
  */
 
-fun Address.withERC55Checksum() = cleanHex.toLowerCase().keccak().toCharArray().let {
+fun Address.withERC55Checksum() = cleanHex.toLowerCase().toByteArray().keccak().toHexString("").let {
     Address(cleanHex.mapIndexed { index, c ->
-        if (c in '0'..'9')
-            c
-        else
-            if (it[index].toLowerCase().fromHexToInt() >= 8) c.toUpperCase() else c.toLowerCase()
+        when {
+            c in '0'..'9' -> c
+            it[index].toLowerCase().fromHexToInt() >= 8 -> c.toUpperCase()
+            else -> c.toLowerCase()
+        }
     }.joinToString(""))
 }
 
