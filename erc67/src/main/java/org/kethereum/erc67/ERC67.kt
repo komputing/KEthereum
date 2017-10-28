4c7f994981c93d2ab91d6c7257f75524cd2a4803
ligi <ligi@ligi.de>
2017-10-28 21:21:00 +0200

    Parse gas value and detect more invalid URLs - closes #5

--- erc67/src/main/java/org/kethereum/erc67/ERC67.kt
+++ erc67/src/main/java/org/kethereum/erc67/ERC67.kt
@@ -4,6 +4,7 @@ import org.kethereum.ETH_IN_WEI
 import org.kethereum.model.Address
 import java.math.BigDecimal
 import java.math.BigInteger
+
 // https://github.com/ethereum/EIPs/issues/67
 
 fun Address.toERC67String() = "ethereum:$hex"
@@ -12,18 +13,40 @@ fun Address.toERC67String(valueInEther: BigDecimal) = toERC67String((valueInEthe
 
 fun String.isERC67String() = startsWith("ethereum:")
 
+private val NUMBER_REGEX = Regex("^[0-9]+$")
+
 class ERC67(val url: String) {
 
     val scheme = url.substringBefore(":")
     private val payload = url.substringAfter(":")
 
     val addressString = payload.substringBefore("?")
-    val query = payload.substringAfter("?","")
-    val queryAsMap = query.split("&").map { it.split("=") }.map { it.first() to it.last() }.toMap()
+    val query = payload.substringAfter("?", "")
+    val queryAsMap = if (query.isBlank()) {
+        emptyMap()
+    } else {
+        query.split("&").map { it.split("=", limit = 2) }.map { it.first() to it[1] }.toMap()
+    }
     val address by lazy { Address(getHex()) }
 
-    fun isValid() = scheme == "ethereum" && url.contains(":")
+    fun isValid(): Boolean {
+        if (scheme != "ethereum" || !url.contains(":")) {
+            return false
+        }
+
+        if (value != null && !NUMBER_REGEX.matches(value!!)) {
+            return false
+        }
+
+        if (gas != null && !NUMBER_REGEX.matches(gas!!)) {
+            return false
+        }
+
+        return true
+    }
+
     fun getHex() = addressString
 
-    fun getValue() = queryAsMap["value"]
+    val value by lazy { queryAsMap["value"] }
+    val gas by lazy { queryAsMap["gas"] }
 }
--- erc67/src/test/java/org/kethereum/erc67/TheERC67.kt
+++ erc67/src/test/java/org/kethereum/erc67/TheERC67.kt
@@ -23,27 +23,33 @@ class TheERC67 {
 
     @Test
     fun invalidERC67IsDetected() {
-        assertThat(ERC67("etheruum:0x00AB42?value=1").isValid()).isEqualTo(false)
+        assertThat(ERC67("etherUUm:0x00AB42?value=1").isValid()).isEqualTo(false)
+        assertThat(ERC67("ethereum:0x00AB42?value=1a&gas=2").isValid()).isEqualTo(false)
+        assertThat(ERC67("ethereum:0x00AB42?value=1&gas=2x").isValid()).isEqualTo(false)
+        assertThat(ERC67("ethereum:0x00AB42?value=1?gas=2").isValid()).isEqualTo(false)
     }
 
     @Test
     fun parsingERC67Works() {
         assertThat(ERC67("ethereum:0x00AB42?value=1").getHex()).isEqualTo("0x00AB42")
-        assertThat(ERC67("ethereum:0x00AB42").getValue()).isNull()
+        assertThat(ERC67("ethereum:0x00AB42").value).isNull()
 
         assertThat(ERC67("ethereum:0x00AB42?value=1").isValid()).isEqualTo(true)
-        assertThat(ERC67("ethereum:0x00AB42?value=1").getValue()).isEqualTo("1")
+        assertThat(ERC67("ethereum:0x00AB42?value=1").value).isEqualTo("1")
 
         (0..10).forEach {
             val probe = ERC67(Address("0xAABB").toERC67String(valueInWei = BigInteger.valueOf(it.toLong())))
-            assertThat(probe.getValue()).isEqualTo(it.toString())
+            assertThat(probe.value).isEqualTo(it.toString())
         }
     }
 
 
     @Test
     fun parsingERC67WithGasValueWorks() {
-        assertThat(ERC67("ethereum:0x00AB42?value=1&gas=2").getValue()).isEqualTo("1")
-        assertThat(ERC67("ethereum:0x00AB42?gas=2&value=1").getValue()).isEqualTo("1")
+        assertThat(ERC67("ethereum:0x00AB42?gas=2").gas).isEqualTo("2")
+        assertThat(ERC67("ethereum:0x00AB42?gas=42&value=3").gas).isEqualTo("42")
+        assertThat(ERC67("ethereum:0x00AB42?value=1&gas=2").value).isEqualTo("1")
+        assertThat(ERC67("ethereum:0x00AB42?gas=2&value=1").gas).isEqualTo("2")
     }
+
 }
