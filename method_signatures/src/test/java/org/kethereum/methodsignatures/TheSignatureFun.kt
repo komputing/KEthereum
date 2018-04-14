65dced5755615383df6f8215a3b23be8e474d8d4
ligi <ligi@ligi.de>
2018-04-14 15:03:55 +0200

    Replace synonyms for function parameters

--- method_signatures/src/test/java/org/kethereum/methodsignatures/TheSignatureFun.kt
+++ method_signatures/src/test/java/org/kethereum/methodsignatures/TheSignatureFun.kt
@@ -12,4 +12,19 @@ class TheSignatureFun {
         assertThat(TextMethodSignature("transfer(address,uint256)").toHexSignature().hex).isEqualTo("a9059cbb")
     }
 
+    @Test
+    fun replacementsOfSynonymsHappen() {
+        assertThat(TextMethodSignature("transfer(address,uint,int,fixed,ufixed)").toHexSignature().hex)
+                .isEqualTo(TextMethodSignature("transfer(address,uint256,int256,fixed128x18,ufixed128x18)").toHexSignature().hex)
+    }
+
+
+    @Test
+    fun replacementsOfSynonymsDontHappenWhenUnsafeUsed() {
+        assertThat(TextMethodSignature("transfer(address,uint,int,fixed,ufixed)").toHexSignatureUnsafe().hex)
+                .isNotEqualTo(TextMethodSignature("transfer(address,uint256,int256,fixed128x18,ufixed128x18)").toHexSignature().hex)
+    }
+
+
+
 }
