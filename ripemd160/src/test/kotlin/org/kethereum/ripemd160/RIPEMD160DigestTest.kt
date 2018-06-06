bc7e7b4ff2d2c48a19356021baa35fb0f3476ac2
ligi <ligi@ligi.de>
2018-06-06 13:50:26 +0200

    Kotlinify a bit more and add helper extension functions

--- ripemd160/src/test/kotlin/org/kethereum/ripemd160/RIPEMD160DigestTest.kt
+++ ripemd160/src/test/kotlin/org/kethereum/ripemd160/RIPEMD160DigestTest.kt
@@ -1,61 +1,46 @@
 package org.kethereum.ripemd160
 
-import org.junit.Assert.assertEquals
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
-import org.walleth.khex.toHexString
+import org.walleth.khex.hexToByteArray
 
-import kotlin.text.Charsets.US_ASCII
 
 class RIPEMD160DigestTest {
 
-    private val messages = arrayOf(
-            "",
-            "a",
-            "abc",
-            "message digest",
-            "abcdefghijklmnopqrstuvwxyz",
-            "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq",
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
-            "12345678901234567890123456789012345678901234567890123456789012345678901234567890"
+    // from http://homes.esat.kuleuven.be/~bosselae/ripemd160.html#Outline
+    private val testVectors = arrayOf(
+            "" to "9c1185a5c5e9fc54612808977ee8f548b2258d31",
+            "a" to "0bdc9d2d256b3ee9daae347be6f4dc835a467ffe",
+            "abc" to "8eb208f7e05d987a9b044a8e98c6b087f15a0bfc",
+            "message digest" to "5d0689ef49d2fae572b881b123a85ffa21595f36",
+            "abcdefghijklmnopqrstuvwxyz" to "f71c27109c692c1b56bbdceb5b9d2865b3708dbc",
+            "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq" to "12a053384a9c0c88e405a06c27dcf49ada62eb2b",
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789" to "b0e20b6e3116640286ed3a87a5713079b21f5189",
+            "1234567890".repeat(8) to "9b752e45573d4b39f4dbd3323cab82bf63326bfb",
+            "a".repeat(1_000_000) to "52783243c1697bdbe16d37f97f68f08325dc1528"
     )
 
-    private val digests = arrayOf(
-            "9c1185a5c5e9fc54612808977ee8f548b2258d31",
-            "0bdc9d2d256b3ee9daae347be6f4dc835a467ffe",
-            "8eb208f7e05d987a9b044a8e98c6b087f15a0bfc",
-            "5d0689ef49d2fae572b881b123a85ffa21595f36",
-            "f71c27109c692c1b56bbdceb5b9d2865b3708dbc",
-            "12a053384a9c0c88e405a06c27dcf49ada62eb2b",
-            "b0e20b6e3116640286ed3a87a5713079b21f5189",
-            "9b752e45573d4b39f4dbd3323cab82bf63326bfb"
-    )
-
-    private val millionADigest = "52783243c1697bdbe16d37f97f68f08325dc1528"
 
     @Test
-    fun simple() {
-        val digest = RIPEMD160Digest()
-        val resBuf = ByteArray(digest.digestLength)
+    fun testStringInput() {
 
-        for (i in 0 until messages.size - 1) {
-            val message = messages[i].toByteArray(US_ASCII)
-            digest.update(message, 0, message.size)
-            digest.doFinal(resBuf, 0)
-
-            assertEquals(digests[i], resBuf.toHexString(""))
+        testVectors.forEach { (message, expectedDigest) ->
+            assertThat(message.calculateRIPEMD160()).isEqualTo(expectedDigest.hexToByteArray())
         }
     }
 
     @Test
-    fun millionA() {
-        val digest = RIPEMD160Digest()
-        val resBuf = ByteArray(digest.digestLength)
+    fun testByteInput() {
 
-        for (i in 0..999999) {
-            digest.update('a'.toByte())
+        val foo = RIPEMD160Digest()
+        (1..1_000_000).forEach {
+            foo.update('a'.toByte())
         }
+        val result = ByteArray(RIPEMD160_DIGEST_LENGTH)
+        foo.doFinal(result, 0)
+        assertThat(result).isEqualTo("52783243c1697bdbe16d37f97f68f08325dc1528".hexToByteArray())
 
-        digest.doFinal(resBuf, 0)
-        assertEquals(millionADigest, resBuf.toHexString(""))
     }
+
+
 }
