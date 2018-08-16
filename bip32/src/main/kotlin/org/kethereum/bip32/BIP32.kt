173505229501b906f8c2dc298bdb5d7fb2785438
ligi <ligi@ligi.de>
2018-08-16 16:21:02 +0200

    More idiomatic code

--- bip32/src/main/kotlin/org/kethereum/bip32/BIP32.kt
+++ bip32/src/main/kotlin/org/kethereum/bip32/BIP32.kt
@@ -4,15 +4,9 @@ package org.kethereum.bip32
 
 import org.kethereum.bip44.BIP44
 
-
-fun generateKey(seed: ByteArray, pathString: String): ExtendedKey {
-    val master = ExtendedKey.createFromSeed(seed)
-
-    var child = master
-    BIP44(pathString).path.forEach {
-        child = child.generateChildKey(it)
-    }
-
-    return child
-}
+fun generateKey(seed: ByteArray, pathString: String) = BIP44(pathString)
+        .path
+        .fold(ExtendedKey.createFromSeed(seed)) { current, biP44Element ->
+            current.generateChildKey(biP44Element)
+        }
 
