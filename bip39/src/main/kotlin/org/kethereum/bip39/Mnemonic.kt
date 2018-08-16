36d8e882966f4f07a1a3d0e5e959b57e218dd451
ligi <ligi@ligi.de>
2018-08-16 15:34:29 +0200

    Use data class and add hashCode fun

--- bip39/src/main/kotlin/org/kethereum/bip39/Mnemonic.kt
+++ bip39/src/main/kotlin/org/kethereum/bip39/Mnemonic.kt
@@ -10,12 +10,13 @@ import javax.crypto.SecretKeyFactory
 import javax.crypto.spec.PBEKeySpec
 
 // TODO inline class once Kotlin 1.3 is out
-class MnemonicWords(val words: Array<String>) {
+data class MnemonicWords(val words: Array<String>) {
     constructor(phrase: String) : this(phrase.split(" "))
     constructor(phrase: List<String>) : this(phrase.toTypedArray())
 
     override fun toString() = words.joinToString(" ")
     override fun equals(other: Any?) = toString() == other?.toString()
+    override fun hashCode() = Arrays.hashCode(words)
 }
 
 fun dirtyPhraseToMnemonicWords(string: String) =  MnemonicWords(string.trim().toLowerCase()
