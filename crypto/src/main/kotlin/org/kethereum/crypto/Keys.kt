574990f95dd906a80fb2638a4dc423ef81686c15
ligi <ligi@ligi.de>
2018-08-21 17:55:27 +0200

    Declare constant

--- crypto/src/main/kotlin/org/kethereum/crypto/Keys.kt
+++ crypto/src/main/kotlin/org/kethereum/crypto/Keys.kt
@@ -21,7 +21,7 @@ const val PRIVATE_KEY_SIZE = 32
 const val PUBLIC_KEY_SIZE = 64
 
 const val ADDRESS_LENGTH_IN_HEX = 40
-val PUBLIC_KEY_LENGTH_IN_HEX = PUBLIC_KEY_SIZE shl 1
+const val PUBLIC_KEY_LENGTH_IN_HEX = PUBLIC_KEY_SIZE shl 1
 
 fun initializeCrypto() {
     Security.insertProviderAt(org.spongycastle.jce.provider.BouncyCastleProvider(), 1)
