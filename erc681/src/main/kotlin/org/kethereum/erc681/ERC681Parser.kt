8e29944c71c4c285d41ebc031b18f37208817a7c
ligi <ligi@ligi.de>
2018-09-04 16:50:18 +0200

    Ability to parse from CommonURI

--- erc681/src/main/kotlin/org/kethereum/erc681/ERC681Parser.kt
+++ erc681/src/main/kotlin/org/kethereum/erc681/ERC681Parser.kt
@@ -1,51 +1,55 @@
 package org.kethereum.erc681
 
 import org.kethereum.model.EthereumURI
+import org.kethereum.uri.common.CommonEthereumURIData
 import org.kethereum.uri.common.parseCommonURI
 import java.math.BigDecimal
 import java.math.BigInteger
 
 private val scientificNumberRegEx = Regex("^[0-9]+(\\.[0-9]+)?(e[0-9]+)?$")
 
-fun EthereumURI.toERC681() = ERC681().apply {
+fun EthereumURI.toERC681() = parseCommonURI().toERC681()
 
-    val commonURI = parseCommonURI()
-    scheme = commonURI.scheme
-    prefix = commonURI.prefix
-    chainId = commonURI.chainId
-    function = commonURI.function
-    address = commonURI.address
-    valid = commonURI.valid
+fun CommonEthereumURIData.toERC681() = let { commonURI ->
+    ERC681().apply {
 
-    fun String?.toBigInteger(): BigInteger? {
-        if (this == null) {
-            return null
-        }
+        scheme = scheme
+        prefix = commonURI.prefix
+        chainId = commonURI.chainId
+        function = commonURI.function
+        address = commonURI.address
+        valid = commonURI.valid
 
-        if (!scientificNumberRegEx.matches(this)) {
-            valid = false
-            return null
-        }
-
-        return when {
-            contains("e") -> {
-                val split = split("e")
-                BigDecimal(split.first()).multiply(BigDecimal.TEN.pow(split[1].toIntOrNull() ?: 1)).toBigInteger()
+        fun String?.toBigInteger(): BigInteger? {
+            if (this == null) {
+                return null
             }
-            contains(".") -> {
+
+            if (!scientificNumberRegEx.matches(this)) {
                 valid = false
-                null
+                return null
             }
-            else -> BigInteger(this)
+
+            return when {
+                contains("e") -> {
+                    val split = split("e")
+                    BigDecimal(split.first()).multiply(BigDecimal.TEN.pow(split[1].toIntOrNull() ?: 1)).toBigInteger()
+                }
+                contains(".") -> {
+                    valid = false
+                    null
+                }
+                else -> BigInteger(this)
+            }
         }
+
+        val queryAsMap = commonURI.query.toMap() // should be improved https://github.com/walleth/kethereum/issues/25
+
+        gas = queryAsMap["gas"].toBigInteger()
+        value = queryAsMap["value"].toBigInteger()
+
+        functionParams = commonURI.query.filter { it.first != "gas" && it.first != "value" }
     }
-
-    val queryAsMap = commonURI.query.toMap() // should be improved https://github.com/walleth/kethereum/issues/25
-
-    gas = queryAsMap["gas"].toBigInteger()
-    value = queryAsMap["value"].toBigInteger()
-
-    functionParams = commonURI.query.filter { it.first != "gas" && it.first != "value" }
 }
 
 fun parseERC681(url: String) = EthereumURI(url).toERC681()
