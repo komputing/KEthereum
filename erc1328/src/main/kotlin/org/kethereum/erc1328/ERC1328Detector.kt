886e8aac615b1289c90ab91c44091fbe2b6c1504
ligi <ligi@ligi.de>
2018-09-21 18:34:37 +0200

    Modify detector to use 831 - makes detecting more efficient

--- erc1328/src/main/kotlin/org/kethereum/erc1328/ERC1328Detector.kt
+++ erc1328/src/main/kotlin/org/kethereum/erc1328/ERC1328Detector.kt
@@ -1,10 +1,10 @@
 package org.kethereum.erc1328
 
+import org.kethereum.erc831.ERC831
+import org.kethereum.erc831.toERC831
 import org.kethereum.model.EthereumURI
-import org.kethereum.uri.common.CommonEthereumURIData
-import org.kethereum.uri.common.parseCommonURI
 
-fun CommonEthereumURIData.isERC1328() =
+fun ERC831.isERC1328() =
         scheme == "ethereum" && prefix == ERC1328_PREFIX
 
-fun EthereumURI.isERC1328() = parseCommonURI().isERC1328()
+fun EthereumURI.isERC1328() = toERC831().isERC1328()
