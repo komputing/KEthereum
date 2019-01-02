12c136f51adc9fcb532ce5744407b874788a28cb
ligi <ligi@ligi.de>
2019-01-02 01:05:59 +0100

    Add module with support for EIP191 - closes #51

---
+++ eip191/src/main/kotlin/org/kethereum/eip191/EIP191.kt
@@ -0,0 +1,19 @@
+package org.kethereum.eip191
+
+import org.kethereum.crypto.model.ECKeyPair
+import org.kethereum.crypto.signMessage
+
+/*
+*
+* http://eips.ethereum.org/EIPS/eip-191
+*
+*/
+
+
+fun ECKeyPair.signWithEIP191(version: Byte, versionSpecificData: ByteArray, message: ByteArray) =
+        signMessage(0x19.toByte().toByteArray() + version.toByteArray() + versionSpecificData + message)
+
+fun ECKeyPair.signWithEIP191PersonalSign(message: ByteArray) =
+        signWithEIP191(0x45, ("thereum Signed Message:\n" + message.size).toByteArray(), message)
+
+private fun Byte.toByteArray() = ByteArray(1) { this }
