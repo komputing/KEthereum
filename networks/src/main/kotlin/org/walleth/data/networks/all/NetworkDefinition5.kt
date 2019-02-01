c29c5be5524214d41e90bd3c01d97aed4f83488d
ligi <ligi@ligi.de>
2019-02-01 18:38:01 +0100

    Add Görli

---
+++ networks/src/main/kotlin/org/walleth/data/networks/all/NetworkDefinition5.kt
@@ -0,0 +1,19 @@
+package org.walleth.data.networks.all
+
+import org.kethereum.model.ChainDefinition
+import org.walleth.data.networks.NetworkDefinition
+
+
+class NetworkDefinition5 : NetworkDefinition {
+
+    override val chain = ChainDefinition(5L, "GOR")
+
+    override fun getNetworkName() = "goerli"
+
+    override val infoUrl = "https://goerli.net/#about"
+
+    override val faucets = listOf(
+            "https://bridge.goerli.com"
+    )
+
+}
