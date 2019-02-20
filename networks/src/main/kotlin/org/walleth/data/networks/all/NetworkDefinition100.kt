dce977148be03671a1d402dbcd6b2bb535796b3e
ligi <ligi@ligi.de>
2019-02-20 14:14:27 +0100

    Add xDAI

---
+++ networks/src/main/kotlin/org/walleth/data/networks/all/NetworkDefinition100.kt
@@ -0,0 +1,16 @@
+package org.walleth.data.networks.all
+
+import org.kethereum.model.ChainDefinition
+import org.kethereum.model.ChainId
+import org.walleth.data.networks.NetworkDefinition
+
+class NetworkDefinition100 : NetworkDefinition {
+
+    override val chain = ChainDefinition(ChainId(100L), "xDAI")
+
+    override fun getNetworkName() = "xDai"
+
+    override val infoUrl = "https://forum.poa.network/c/xdai-chain"
+
+    override val faucets = emptyList<String>()
+}
