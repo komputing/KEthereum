c923fd5ec612c17b67704096f804a433a02c5b74
ligi <ligi@ligi.de>
2019-02-24 18:01:24 +0100

    Fix ChainId toString()

--- model/src/main/kotlin/org/kethereum/model/ChainDefinition.kt
+++ model/src/main/kotlin/org/kethereum/model/ChainDefinition.kt
@@ -3,5 +3,5 @@ package org.kethereum.model
 data class ChainDefinition(
         val id: ChainId,
         private val prefix: String) {
-    override fun toString() = "$prefix:$id"
+    override fun toString() = "$prefix:${id.value}"
 }
