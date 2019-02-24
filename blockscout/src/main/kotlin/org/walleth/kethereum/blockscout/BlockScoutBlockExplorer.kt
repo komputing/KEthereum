cc644ef3abf311d90e35d6cd7010022e22da7c57
ligi <ligi@ligi.de>
2019-02-24 23:29:09 +0100

    Add blockscout

---
+++ blockscout/src/main/kotlin/org/walleth/kethereum/blockscout/BlockScoutBlockExplorer.kt
@@ -0,0 +1,14 @@
+package org.walleth.kethereum.blockscout
+
+import org.kethereum.model.Address
+import org.kethereum.model.BlockExplorer
+
+class BlockScoutBlockExplorer(private val path: String?) : BlockExplorer {
+
+    private val baseURL by lazy { "https://blockscout.com/$path" }
+
+    override fun getAddressURL(address: Address) = "$baseURL/address/${address.hex}"
+    override fun getTransactionURL(transactionHash: String) = "$baseURL/tx/$transactionHash"
+    override fun getBlockURL(blockNum: Long) = "$baseURL/block/$blockNum"
+
+}
