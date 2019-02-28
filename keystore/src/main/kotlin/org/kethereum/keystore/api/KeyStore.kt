c3872753354940f7fbe1dccad58783ad11906c73
ligi <ligi@ligi.de>
2019-03-01 00:13:42 +0100

    Make password optional with default parameter

--- keystore/src/main/kotlin/org/kethereum/keystore/api/KeyStore.kt
+++ keystore/src/main/kotlin/org/kethereum/keystore/api/KeyStore.kt
@@ -4,9 +4,9 @@ import org.kethereum.model.Address
 import org.kethereum.model.ECKeyPair
 
 interface KeyStore {
-    fun addKey(key: ECKeyPair, password: String, light: Boolean = false): Address?
+    fun addKey(key: ECKeyPair, password: String = "", light: Boolean = false): Address?
     fun deleteKey(address: Address): Boolean
-    fun getKeyForAddress(address: Address, password: String): ECKeyPair?
+    fun getKeyForAddress(address: Address, password: String = ""): ECKeyPair?
     fun hasKeyForForAddress(address: Address): Boolean
     fun getAddresses(): Set<Address>
 }
