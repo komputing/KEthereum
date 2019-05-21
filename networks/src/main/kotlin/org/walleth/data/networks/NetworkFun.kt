ac3d5dced664c471a47885c01e6e01e28d861776
ligi <ligi@ligi.de>
2019-05-21 15:51:27 +0200

    Remove networks - should be taken from ethereum-lists/chains

--- networks/src/main/kotlin/org/walleth/data/networks/NetworkFun.kt
+++
@@ -1,7 +0,0 @@
-package org.walleth.data.networks
-
-import org.kethereum.model.ChainId
-
-fun getNetworkDefinitionByChainID(chainID: ChainId) = ALL_NETWORKS.firstOrNull { it.chain.id == chainID }
-
-fun ChainId.findNetworkDefinition() = ALL_NETWORKS.firstOrNull { it.chain.id == this }
