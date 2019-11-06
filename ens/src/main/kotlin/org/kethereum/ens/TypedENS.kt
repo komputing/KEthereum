f5176841e224ecd7c863dbbb168dd205122453ff
ligi <ligi@ligi.de>
2019-11-06 15:10:46 +0900

    Initial ENS support

---
+++ ens/src/main/kotlin/org/kethereum/ens/TypedENS.kt
@@ -0,0 +1,26 @@
+package org.kethereum.ens
+
+import org.kethereum.eip137.ENSName
+import org.kethereum.eip137.toNameHashByteArray
+import org.kethereum.ens.generated.ENS
+import org.kethereum.ens.generated.Resolver
+import org.kethereum.model.Address
+import org.kethereum.rpc.EthereumRPC
+
+class TypedENS(private val rpc: EthereumRPC,
+               ensAddress: Address) {
+
+    private val ens = ENS(rpc, ensAddress)
+
+    fun getResolver(name: ENSName) = ens.resolver(name.toNameHashByteArray())
+
+    fun getAddress(name: ENSName): Address? {
+        val bytes32 = name.toNameHashByteArray()
+        val resolver = ens.resolver(bytes32)
+        return if (resolver != null) {
+            return Resolver(rpc, resolver).addr(bytes32)
+        } else {
+            null
+        }
+    }
+}
