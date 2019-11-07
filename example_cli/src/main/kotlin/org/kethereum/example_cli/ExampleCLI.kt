5e2986ad5aa70c5ea5d9f05f94794f93ac2d3815
ligi <ligi@ligi.de>
2019-11-07 19:04:52 +0900

    Also demo ENS in example CLI

--- example_cli/src/main/kotlin/org/kethereum/example_cli/ExampleCLI.kt
+++ example_cli/src/main/kotlin/org/kethereum/example_cli/ExampleCLI.kt
@@ -3,12 +3,22 @@ package org.kethereum.example_cli
 import org.kethereum.eip137.ENSName
 import org.kethereum.eip137.toHexString
 import org.kethereum.eip137.toNameHash
+import org.kethereum.ens.TypedENS
 import org.kethereum.erc55.withERC55Checksum
 import org.kethereum.model.Address
+import org.kethereum.rpc.HttpEthereumRPC
+
+val rpc = HttpEthereumRPC("https://node3.web3api.com")
+val ens = TypedENS(rpc, Address("0x314159265dd8dbb310642f98f50c066173c1259b"))
 
 fun main() {
     demoERC55()
     demoEIP137()
+    demoENS()
+}
+
+fun demoENS() {
+    println("ligi.ethereum.eth -> " + ens.getAddress(ENSName("ligi.ethereum.eth")))
 }
 
 private fun demoEIP137() {
