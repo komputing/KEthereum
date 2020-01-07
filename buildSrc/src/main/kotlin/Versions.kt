b01ac83cc552bcea170b83565ab0702de17c0620
ligi <ligi@ligi.de>
2020-01-07 17:32:32 +0100

    Use slf4j 1.7.30

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,7 +5,7 @@ object Versions {
     const val moshi = "1.8.0"
     const val khex = "1.0.0-RC6"
     const val khash = "1.0.0-RC5"
-    const val slf4j = "1.7.29"
+    const val slf4j = "1.7.30"
     const val klaxon = "5.2"
     const val okhttp= "3.12.1"
 }
