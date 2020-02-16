a2061a98de6d075f404ab7a3fcdacdc8333a277b
ligi <ligi@ligi.de>
2020-02-16 11:31:56 +0100

    Make ID and constant fields optional and test with more contracts

--- abi/src/main/kotlin/org/kethereum/abi/model/EthereumABIElement.kt
+++ abi/src/main/kotlin/org/kethereum/abi/model/EthereumABIElement.kt
@@ -2,8 +2,8 @@ package org.kethereum.abi.model
 
 data class EthereumABIElement(
         val name: String?,
-        val id: String,
-        val constant: Boolean,
+        val id: String?,
+        val constant: Boolean?,
         val inputs: List<EthereumNamedType>?,
         val outputs: List<EthereumNamedType>?,
         val result: String?,
--- abi/src/test/kotlin/org/kethereum/abi/TheParser.kt
+++ abi/src/test/kotlin/org/kethereum/abi/TheParser.kt
@@ -7,7 +7,14 @@ class TheParser {
 
     @Test
     fun canParseABI() {
-        val parsed = EthereumABI(javaClass.getResource("/peepeth.abi").readText())
-        assertThat(parsed.methodList.size).isEqualTo(31)
+        listOf("/peepeth.abi" to 31,
+                "/ENS.abi" to 11,
+                "/ENSResolver.abi" to 29,
+                "/MCD.abi" to 8,
+                "/ERC20.abi" to 12
+        ).forEach {
+            val parsed = EthereumABI(javaClass.getResource(it.first).readText())
+            assertThat(parsed.methodList.size).isEqualTo(it.second)
+        }
     }
 }
