c72aa3d0701c3a883276bf64443156dc9998b0cc
Alexander Tarasenko <alexander.tarasenko@alpeinsoft.ch>
2020-04-09 02:02:38 +0300

    Support EIP-1191

--- erc55/src/main/kotlin/org/kethereum/erc55/ERC55.kt
+++ erc55/src/main/kotlin/org/kethereum/erc55/ERC55.kt
@@ -2,14 +2,23 @@ package org.kethereum.erc55
 
 import org.kethereum.keccakshortcut.keccak
 import org.kethereum.model.Address
+import org.kethereum.model.ChainId
 import org.komputing.khex.extensions.toNoPrefixHexString
 import java.util.*
 
 /*
 ERC-55 Checksum as in https://github.com/ethereum/EIPs/blob/master/EIPS/eip-55.md
+https://github.com/ethereum/EIPs/blob/master/EIPS/eip-1191.md
  */
 
-fun Address.withERC55Checksum() = cleanHex.toLowerCase(Locale.ROOT).toByteArray().keccak().toNoPrefixHexString().let { hexHash ->
+private fun Address.cleanHexWithPrefix(chainId: ChainId?) =
+        if (chainId != null) {
+            "${chainId.value}0x${cleanHex}"
+        } else {
+            cleanHex
+        }
+
+fun Address.withERC55Checksum(chainId: ChainId? = null) = cleanHexWithPrefix(chainId).toLowerCase(Locale.ROOT).toByteArray().keccak().toNoPrefixHexString().let { hexHash ->
     Address(cleanHex.mapIndexed { index, hexChar ->
         when {
             hexChar in '0'..'9' -> hexChar
@@ -19,10 +28,10 @@ fun Address.withERC55Checksum() = cleanHex.toLowerCase(Locale.ROOT).toByteArray(
     }.joinToString(""))
 }
 
-private fun Address.hasValidERC55ChecksumAssumingValidAddress() = withERC55Checksum().hex == hex
+private fun Address.hasValidERC55ChecksumAssumingValidAddress(chainId: ChainId? = null) = withERC55Checksum(chainId).hex == hex
 
-fun Address.hasValidERC55Checksum() = isValid() && hasValidERC55ChecksumAssumingValidAddress()
-fun Address.hasValidERC55ChecksumOrNoChecksum() = isValid() &&
-        (hasValidERC55ChecksumAssumingValidAddress() ||
+fun Address.hasValidERC55Checksum(chainId: ChainId? = null) = isValid() && hasValidERC55ChecksumAssumingValidAddress(chainId)
+fun Address.hasValidERC55ChecksumOrNoChecksum(chainId: ChainId? = null) = isValid() &&
+        (hasValidERC55ChecksumAssumingValidAddress(chainId) ||
                 cleanHex.toLowerCase() == cleanHex ||
                 cleanHex.toUpperCase() == cleanHex)
