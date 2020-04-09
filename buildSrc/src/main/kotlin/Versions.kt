600dd4bb78a18d9cffca3ce1ee5f72fbb03f0e90
ligi <ligi@ligi.de>
2020-04-09 03:22:55 +0200

    Use threetenbp 1.4.3

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -9,7 +9,7 @@ object Versions {
     const val klaxon = "5.2"
     const val okhttp = "3.12.1"
     const val coroutines = "1.3.4"
-    const val threetenbp = "1.4.2"
+    const val threetenbp = "1.4.3"
     const val okio = "2.4.0"
     const val base58 = "0.1"
 }
