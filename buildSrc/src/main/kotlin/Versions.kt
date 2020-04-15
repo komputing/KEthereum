1b07db188e6e32dc272e689927f1ae607bee55e8
ligi <ligi@ligi.de>
2020-04-16 00:06:42 +0200

    Use Kotlin 1.3.72

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,5 +1,5 @@
 object Versions {
-    const val kotlin = "1.3.71"
+    const val kotlin = "1.3.72"
     const val versions_plugin = "0.28.0"
     const val jupiter = "5.6.1"
     const val moshi = "1.8.0"
