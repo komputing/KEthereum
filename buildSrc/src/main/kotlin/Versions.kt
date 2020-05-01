d97725c13b61621d5f07b0c80e9ebea32c18b1d3
ligi <ligi@ligi.de>
2020-05-01 03:57:36 +0200

    Use jupiter 5.6.2

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
     const val kotlin = "1.3.72"
     const val versions_plugin = "0.28.0"
-    const val jupiter = "5.6.1"
+    const val jupiter = "5.6.2"
     const val moshi = "1.8.0"
     const val khex = "1.0.0-RC6"
     const val khash = "1.0.0-RC5"
