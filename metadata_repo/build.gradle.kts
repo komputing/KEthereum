35d41362600e7b3516310e3920f4055feed0a964
ligi <ligi@ligi.de>
2020-05-12 06:16:07 +0200

    Add function to get by tx and add missing tests

--- metadata_repo/build.gradle.kts
+++ metadata_repo/build.gradle.kts
@@ -4,4 +4,6 @@ dependencies {
     implementation(project(":erc55"))
     implementation("com.squareup.okhttp3:okhttp:${Versions.okhttp}")
 
+    testImplementation(project(":test_data"))
+    testImplementation("com.squareup.okhttp3:mockwebserver:${Versions.okhttp}")
 }
