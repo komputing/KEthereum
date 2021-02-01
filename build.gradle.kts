f884f775dfa85aa5889a064a212f474ec7caea02
ligi <ligi@ligi.de>
2021-02-01 19:53:26 +0100

    Use assertj 3.19.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ subprojects {
         dependencies {
             "implementation"("org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}")
 
-            "testImplementation"("org.assertj:assertj-core:3.18.1")
+            "testImplementation"("org.assertj:assertj-core:3.19.0")
             "testImplementation"("org.junit.jupiter:junit-jupiter-api:${Versions.jupiter}")
             "testImplementation"("org.junit.jupiter:junit-jupiter-params:${Versions.jupiter}")
             "testRuntime"("org.junit.jupiter:junit-jupiter-engine:${Versions.jupiter}")
