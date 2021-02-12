9ac021cb8c01e333c417f0e82ff5058ad73ba320
ligi <ligi@ligi.de>
2021-02-12 18:05:51 +0100

    Use jupiter 5.7.1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
     const val kotlin = "1.4.21"
     const val versions_plugin = "0.36.0"
-    const val jupiter = "5.7.0"
+    const val jupiter = "5.7.1"
     const val moshi = "1.8.0"
     const val khex = "1.0.0"
     const val khash = "1.0.0-RC5"
