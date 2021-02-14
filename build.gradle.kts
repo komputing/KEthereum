237edda2e373af7c0fe123e5ab92b7cbd1654b1c
ligi <ligi@ligi.de>
2021-02-14 23:28:48 +0100

    Improve setting of jvmTarget
    intValueExact does not seem to be available on Android

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,7 +52,14 @@ subprojects {
 
 
     }
-    val compileTestKotlin by tasks.getting(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class) {
-        kotlinOptions.jvmTarget = "1.8"
+
+    val compileKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
+    val compileTestKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
+
+    compileKotlin.kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    compileTestKotlin.kotlinOptions {
+        jvmTarget = "1.8"
     }
 }
