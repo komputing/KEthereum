21a001ec0ab3b7b18811c4b54aab1a24d412c40d
Abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2021-03-09 23:42:46 +0530

    updated bcprov version

--- crypto_impl_bouncycastle/build.gradle.kts
+++ crypto_impl_bouncycastle/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
-    "implementation"("org.bouncycastle:bcprov-jdk15on:1.67")
+    "implementation"("org.bouncycastle:bcprov-jdk15to18:1.68")
     "implementation"(project(":crypto_api"))
     "implementation"(project(":crypto_impl_java_provider"))
     "implementation"(project(":model"))
