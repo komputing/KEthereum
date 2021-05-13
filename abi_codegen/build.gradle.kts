284e8b20b5e77b7d8a7f25dc6f2a941192014382
ligi <ligi@ligi.de>
2021-05-14 01:01:17 +0200

    Use Kotlinpoet 1.8.0

--- abi_codegen/build.gradle.kts
+++ abi_codegen/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
     "implementation"("org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin}")
 
     "api"("com.squareup.moshi:moshi:${Versions.moshi}")
-    "api"("com.squareup:kotlinpoet:1.7.2")
+    "api"("com.squareup:kotlinpoet:1.8.0")
 
     "testImplementation"(project(":test_data"))
     "testImplementation"(project(":extensions_kotlin"))
