e626e32672a86cce124e0bce38446f2bc0675a6e
ligi <ligi@ligi.de>
2021-05-13 22:46:53 +0200

    Migrate to kbip44 0.4

--- bip32/build.gradle.kts
+++ bip32/build.gradle.kts
@@ -3,7 +3,7 @@ dependencies {
     implementation(project(":crypto_api"))
     implementation(project(":extensions_kotlin"))
     implementation(project(":model"))
-    implementation("com.github.komputing:kbip44:0.1")
+    implementation("com.github.komputing:kbip44:0.4")
     implementation("com.github.komputing:kbase58:${Versions.base58}")
     implementation("com.github.komputing.khash:sha256:${Versions.khash}")
     implementation("com.github.komputing.khash:ripemd160:${Versions.khash}")
--- bip32/src/test/kotlin/org/kethereum/bip32/DerivationTest.kt
+++ bip32/src/test/kotlin/org/kethereum/bip32/DerivationTest.kt
@@ -16,7 +16,7 @@ class DerivationTest {
 
     @Test
     fun createdKeyHasKeypair() {
-        val simpleKey = Seed(ByteArray(0)).toKey("m/")
+        val simpleKey = Seed(ByteArray(0)).toKey("m/0")
         assertThat(simpleKey.keyPair).isNotNull
     }
 
@@ -58,7 +58,7 @@ class DerivationTest {
     private val testData = arrayOf(
             DerivationVector(
                     "000102030405060708090a0b0c0d0e0f",
-                    "m/",
+                    "m",
                     "xpub661MyMwAqRbcFtXgS5sYJABqqG9YLmC4Q1Rdap9gSE8NqtwybGhePY2gZ29ESFjqJoCu1Rupje8YtGqsefD265TMg7usUDFdp6W1EGMcet8",
                     "xprv9s21ZrQH143K3QTDL4LXw2F7HEK3wJUD2nW2nRk4stbPy6cq3jPPqjiChkVvvNKmPGJxWUtg6LnF5kejMRNNU3TGtRBeJgk33yuGBxrMPHi",
                     "tpubD6NzVbkrYhZ4XgiXtGrdW5XDAPFCL9h7we1vwNCpn8tGbBcgfVYjXyhWo4E1xkh56hjod1RhGjxbaTLV3X4FyWuejifB9jusQ46QzG87VKp",
@@ -112,7 +112,7 @@ class DerivationTest {
 
             DerivationVector(
                     "fffcf9f6f3f0edeae7e4e1dedbd8d5d2cfccc9c6c3c0bdbab7b4b1aeaba8a5a29f9c999693908d8a8784817e7b7875726f6c696663605d5a5754514e4b484542",
-                    "m/",
+                    "m",
                     "xpub661MyMwAqRbcFW31YEwpkMuc5THy2PSt5bDMsktWQcFF8syAmRUapSCGu8ED9W6oDMSgv6Zz8idoc4a6mr8BDzTJY47LJhkJ8UB7WEGuduB",
                     "xprv9s21ZrQH143K31xYSDQpPDxsXRTUcvj2iNHm5NUtrGiGG5e2DtALGdso3pGz6ssrdK4PFmM8NSpSBHNqPqm55Qn3LqFtT2emdEXVYsCzC2U",
                     "tpubD6NzVbkrYhZ4XJDrzRvuxHEyQaPd1mwwdDofEJwekX18tAdsqeKfxss79AJzg1431FybXg5rfpTrJF4iAhyR7RubberdzEQXiRmXGADH2eA",
@@ -166,7 +166,7 @@ class DerivationTest {
 
             DerivationVector(
                     "4b381541583be4423346c643850da4b320e46a87ae3d2a4e6da11eba819cd4acba45d239319ac14f863b8d5ab5a0d0c64d2e8a1e7d1457df2e5a3c51c73235be",
-                    "m/",
+                    "m",
                     "xpub661MyMwAqRbcEZVB4dScxMAdx6d4nFc9nvyvH3v4gJL378CSRZiYmhRoP7mBy6gSPSCYk6SzXPTf3ND1cZAceL7SfJ1Z3GC8vBgp2epUt13",
                     "xprv9s21ZrQH143K25QhxbucbDDuQ4naNntJRi4KUfWT7xo4EKsHt2QJDu7KXp1A3u7Bi1j8ph3EGsZ9Xvz9dGuVrtHHs7pXeTzjuxBrCmmhgC6",
                     "tpubD6NzVbkrYhZ4WMg2WpRiAGW1HDiime7DLZaDdbyD2D5vrQs9VnZdv96dd9qyVbdgBLjTMfxs4VHhjYhd1R1rXmZjitkrinrNW9HDndbLQPW",
