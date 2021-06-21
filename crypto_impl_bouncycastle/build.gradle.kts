1f8340515dda427c81c306378c86752e7a476677
ligi <ligi@ligi.de>
2021-06-21 23:48:16 +0200

    Use bouncycastle 1.69

--- crypto_impl_bouncycastle/build.gradle.kts
+++ crypto_impl_bouncycastle/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
-    "implementation"("org.bouncycastle:bcprov-jdk15to18:1.68")
+    "implementation"("org.bouncycastle:bcprov-jdk15to18:1.69")
     "implementation"(project(":crypto_api"))
     "implementation"(project(":crypto_impl_java_provider"))
     "implementation"(project(":model"))
