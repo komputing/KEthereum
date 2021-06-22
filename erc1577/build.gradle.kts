fea167b8e81500ce0807ecbaf99b730a6e62c719
ligi <ligi@ligi.de>
2021-06-22 07:34:21 +0200

    Use kotlin-multihash 0.2

--- erc1577/build.gradle.kts
+++ erc1577/build.gradle.kts
@@ -1,15 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-tasks.withType<KotlinCompile>().all {
-    kotlinOptions.freeCompilerArgs += listOf("-XXLanguage:+InlineClasses","-Xopt-in=kotlin.ExperimentalUnsignedTypes")
-}
-
 dependencies {
     implementation("com.github.komputing:khex:${Versions.khex}")
     implementation("com.squareup.okio:okio:${Versions.okio}")
     implementation("com.github.komputing:kbase58:${Versions.base58}")
-    implementation("com.github.komputing:kvarint:0.2")
+    implementation("com.github.komputing:kvarint:0.3")
 
-    implementation("com.github.komputing:kotlin-multihash:6d9827e098ae2990f4273db111a24e6cc2572f04")
+    implementation("com.github.komputing:kotlin-multihash:0.2")
     implementation("com.github.komputing:khex:${Versions.khex}")
 }
