b051170645125bc35660a19603096bff97becbbf
ligi <ligi@ligi.de>
2021-06-25 14:25:39 +0200

    Use modularized deps to prevent problems consuming the lib

--- erc1577/build.gradle.kts
+++ erc1577/build.gradle.kts
@@ -4,8 +4,8 @@ dependencies {
     implementation("com.github.komputing:khex:${Versions.khex}")
     implementation("com.squareup.okio:okio:${Versions.okio}")
     implementation("com.github.komputing:kbase58:${Versions.base58}")
-    implementation("com.github.komputing:kvarint:0.3")
+    implementation("com.github.komputing:kvarint:0.4")
 
-    implementation("com.github.komputing:kotlin-multihash:0.2")
+    implementation("com.github.komputing:kotlin-multihash:0.3")
     implementation("com.github.komputing:khex:${Versions.khex}")
 }
