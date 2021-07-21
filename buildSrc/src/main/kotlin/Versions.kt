d13715329042fde46f47bcb026190174b19b9e39
ligi <ligi@ligi.de>
2021-07-21 13:53:08 +0200

    Use coroutines 1.5.1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -8,7 +8,7 @@ object Versions {
     const val slf4j = "1.7.31"
     const val klaxon = "5.5"
     const val okhttp = "3.12.1"
-    const val coroutines = "1.5.0"
+    const val coroutines = "1.5.1"
     const val threetenbp = "1.5.1"
     const val okio = "2.4.0"
     const val base58 = "0.4"
