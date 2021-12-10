f7f965f7b5d1deb5c087c805af08a6be38bb22f1
ligi <ligi@ligi.de>
2021-12-10 22:48:35 +0100

    Use threetenbp 1.5.2

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -9,7 +9,7 @@ object Versions {
     const val klaxon = "5.5"
     const val okhttp = "3.12.1"
     const val coroutines = "1.5.1"
-    const val threetenbp = "1.5.1"
+    const val threetenbp = "1.5.2"
     const val okio = "2.4.0"
     const val base58 = "0.4"
 }
