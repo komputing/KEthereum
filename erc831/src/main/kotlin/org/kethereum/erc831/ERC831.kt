408686b90ab40b6208ae92a314e205052572f473
Gleb Levinkov <bigman212@yandex.ru>
2023-10-14 16:04:44 +0800

    Add EIP961 doc for :erc961 module

--- erc831/src/main/kotlin/org/kethereum/erc831/ERC831.kt
+++ erc831/src/main/kotlin/org/kethereum/erc831/ERC831.kt
@@ -1,9 +1,10 @@
 package org.kethereum.erc831
 
-// as defined in http://eips.ethereum.org/EIPS/eip-831
-
+/**
+ * as defined in [EIP-831](https://eips.ethereum.org/EIPS/eip-831)
+ */
 data class ERC831(
-        var scheme: String? = null,
-        var prefix: String? = null,
-        var payload: String? = null
+    var scheme: String? = null,
+    var prefix: String? = null,
+    var payload: String? = null
 )
--- erc961/src/main/kotlin/org/kethereum/erc961/ERC961Parser.kt
+++ erc961/src/main/kotlin/org/kethereum/erc961/ERC961Parser.kt
@@ -11,8 +11,11 @@ class InvalidTokenURIException : IllegalArgumentException("invalid token")
 private const val TOKEN_INFO_PREFIX = "token_info"
 private const val FULL_URI_PREFIX = "ethereum:$TOKEN_INFO_PREFIX"
 
-fun isEthereumTokenURI(uri: String) = uri.startsWith(FULL_URI_PREFIX)
+fun isEthereumTokenURI(uri: String): Boolean = uri.startsWith(FULL_URI_PREFIX)
 
+/**
+ * @throws InvalidTokenURIException if the URL doesn't implement EIP-961 standard
+ */
 fun parseTokenFromEthereumURI(uri: String): Token {
     val commonEthereumURI = EthereumURI(uri).parseCommonURI()
     val queryMap = commonEthereumURI.query.toMap()
@@ -23,7 +26,7 @@ fun parseTokenFromEthereumURI(uri: String): Token {
 
     return Token(
         symbol = queryMap["symbol"] ?: "SYM",
-        address = Address(commonEthereumURI.address ?: ""),
+        address = Address(commonEthereumURI.address.orEmpty()),
         chain = commonEthereumURI.chainId ?: ChainId(1),
         name = queryMap["name"],
         decimals = queryMap["decimals"]?.toInt() ?: 18,
@@ -31,5 +34,5 @@ fun parseTokenFromEthereumURI(uri: String): Token {
     )
 }
 
-fun EthereumURI.parseToken() = parseTokenFromEthereumURI(uri)
-fun EthereumURI.isTokenURI() = isEthereumTokenURI(uri)
+fun EthereumURI.parseToken(): Token = parseTokenFromEthereumURI(uri)
+fun EthereumURI.isTokenURI(): Boolean = isEthereumTokenURI(uri)
