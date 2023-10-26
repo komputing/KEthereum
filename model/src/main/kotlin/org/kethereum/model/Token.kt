1f42cede2d31cb5d3c488bd74eeb8480ec47c919
Gleb <bigman212@yandex.ru>
2023-10-26 09:34:51 +0800

    Add Readme and some tests to :erc961 (#157)
    * Add EIP961 doc for :erc961 module
    
    * Add EIP961 extensions tests

--- model/src/main/kotlin/org/kethereum/model/Token.kt
+++ model/src/main/kotlin/org/kethereum/model/Token.kt
@@ -1,8 +1,10 @@
 package org.kethereum.model
 
-data class Token(val symbol: String,
-                 val address: Address,
-                 val chain: ChainId,
-                 val decimals: Int = 18,
-                 val type: String? = null,
-                 val name: String? = null)
+data class Token(
+    val symbol: String,
+    val address: Address,
+    val chain: ChainId,
+    val decimals: Int = 18,
+    val type: String? = null,
+    val name: String? = null
+)
